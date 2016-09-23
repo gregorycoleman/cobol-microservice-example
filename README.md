77a7795fe79d50297385fb77333152b1ffe2a4bb
wenzi <wenzi@nodealarm-ubuntu-512mb-sfo2-01>
2016-09-23 07:00:45 +0000

    Moving to github

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 
+Cobol Microservices Example
+Copyright (C) 2016 Gregory Coleman
+
 
 docker build -t cobol-microservice-example  .
 
 docker run -it -p 7707:7707  cobol-microservice-example  bash
+
+docker run -it -p 7707:7707   gregcoleman/cobol-microservice-example  bash
 
  curl -H "Content-Type: application/json" http://127.0.0.1:7707
 
--- src/index.php
+++ src/index.php
@@ -1,12 +1,32 @@
 <?php
+/*
+
+Cobol Microservices Example
+Copyright (C) 2016 Gregory Coleman
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
 /**
+
  * This is only an example
  * If this were a production use, you would have to include all the
  * usual things like error blocks and signal handling.
 
   */
 
-//print_r($_SERVER) ;
 $method = "GET" ;   // default to GET if calling from command  line
 
 if (isset($_SERVER['REQUEST_METHOD'])) {
